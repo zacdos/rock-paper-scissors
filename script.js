16f367c37ee371d36da9f06ef2c4cb2db05fce39
zacdos <zja.dpt@gmail.com>
2025-02-15 17:52:06 -0800

    switched string to number conversions from human to computer

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-function humanChoice() {
+function getHumanChoice() {
     let choice = prompt('rock paper or scissors?');
-    if (choice === "rock") {
-        console.log(1);
-        return 1;
-    } else if (choice === "paper") {
-        console.log(2);
-        return 2;
-    } else if (choice === "scissors") {
-        console.log(3);
-        return 3;
-    } else {
-        console.log(null);
-        return null;
+}
+
+function getComputerChoice(max = 3) {
+    const computerSelection = Math.floor(Math.random() * 3 + 1);
+    console.log('Computer plays: ');
+    if (computerSelection === 1) {
+        console.log('rock');
+        return 'rock';
+    } else if (computerSelection === 2) {
+        console.log('paper');
+        return 'paper';
+    } else if (computerSelection === 3) {
+        console.log('scissors');
+        return 'scissors';
     }
 }
 
-humanChoice();
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+function playRound() {
+    if(humanSelection === computerSelection) {
+        console.log('tie!');
+    } else if (humanSelection === 'rock' && computerSelection === 'scissors') {
+        console.log('you win');
+    } else if (humanSelection === 'paper' && computerSelection === 'rock') {
+        console.log('you win');
+    } else if (humanSelection === 'scissors' && computerSelection === 'paper') {
+        console.log('you win');
+    } else {
+        console.log('computer wins!')
+    }
+ } 
+playRound()
