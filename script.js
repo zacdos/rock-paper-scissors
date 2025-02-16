4c087abb8e4f7d096ea21e4b2c6b9feff7440574
zacdos <zja.dpt@gmail.com>
2025-02-15 17:36:18 -0800

    added more efficient IF ELSE statements to pseudocode and redid script

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-function getComputerChoice(max = 3) {
-    const computerSelection = Math.floor(Math.random() * 3 + 1);
-    console.log('Computer plays: ' + computerSelection);
-    return computerSelection;
-  }
-  
- function getHumanChoice() {
-    const choice = prompt('rock paper or scissors?');
-    console.log(choice);
-    return choice;
- }
-
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
-function playRound(humanSelection, computerSelection) {
-    getHumanChoice();
-    getComputerChoice();
-    if(humanSelection < computerSelection) {
-        console.log('you lose');
-    } else if (humanSelection > computerSelection) {
-        console.log('you win');
+function humanChoice() {
+    let choice = prompt('rock paper or scissors?');
+    if (choice === "rock") {
+        console.log(1);
+        return 1;
+    } else if (choice === "paper") {
+        console.log(2);
+        return 2;
+    } else if (choice === "scissors") {
+        console.log(3);
+        return 3;
     } else {
-        console.log('tie!')
+        console.log(null);
+        return null;
     }
- }  
- playRound();
+}
+
+humanChoice();
