ed87e41a8c77a216a59af98dc9dad879ecd61ff9
zacdos <zja.dpt@gmail.com>
2025-02-17 10:47:56 -0800

    coded humanChoice variable to convert to all lowercase to be able to compare to computerChoice

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ let computerScore = 0;
 
 function playRound() {
     for (i=0; i<=99; i++) {
-        const humanChoice = prompt('rock, paper, or scissors?'); 
+        let humanPrompt = prompt('rock, paper, or scissors?'); 
+        let humanChoice = humanPrompt.toLowerCase();
         console.log('Human plays ' + humanChoice);
         let computerChoice = Math.floor(Math.random() * 3 + 1);
             if (computerChoice === 1) {
-                console.log('rock');
                 computerChoice = 'rock';
             } else if (computerChoice === 2) {
-                console.log('paper');
                 computerChoice = 'paper';
             } else if (computerChoice === 3) {
-                console.log('scissors');
                 computerChoice = 'scissors';
             };
         console.log('Computer plays ' + computerChoice)
@@ -35,11 +33,11 @@ function playRound() {
         }
         console.log('SCORES: Human ' + humanScore + ', Computer ' + computerScore);
         if (humanScore === 5) {
-            console.log('Human wins the match!')
+            console.log('HUMAN WINS THE ENTIRE MATCH!')
             break;
         }
         if (computerScore === 5) {
-            console.log('Computer wins the match!')
+            console.log('COMPUTER WINS THE ENTIRE MATCH!')
             break;
         }
     }
